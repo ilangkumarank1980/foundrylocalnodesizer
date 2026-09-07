23cf58f80e9187d57c3bd7fa77ad22b572fd3335
Ilan <mandi2000in@yahoo.com>
2026-09-07 10:16:21 -0500

    Add header for content type in index.php
    Set content type for HTML presentation.

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php include_once("index.html"); ?>
+<?php
+// Tells Azure and the browser to treat this exactly as a clean, styled HTML web presentation page
+header("Content-Type: text/html; charset=UTF-8");
+include_once("index.html");
+?>
